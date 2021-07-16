989d2cbc06a145e0af9d0bb784f4aef7897c25b8
Matthew Sugden <mmsugden@gmail.com>
2021-07-16 15:05:53 +0000

    Did the tasks, have some queries

--- main.cpp
+++ main.cpp
@@ -15,16 +15,15 @@ video: Chapter 2 - Part 3
  
  
  1) Write down the names of all of the primitives available in C++ (excluding wchar_t) here: 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+(assuming not needing signed, unsigned, short and long variations as well as wchar_t?) ...
+
+ bool
+ char
+ int
+ float
+ double
+ void
  
 2) for each primitive type, write out 3 variable declarations inside the variableDeclaration() function on line 59.
     a) give each variable declaration an initial value
@@ -58,12 +57,28 @@ video: Chapter 2 - Part 3
 //2)
 void variableDeclarations()
 {
-    //example:
-    int number = 2; //declaration of a variable named "number", that uses the primitive type 'int', and the variable's initial value is '2'
-    
+    bool isOn = 0;
+    bool isHigherThan5 = 1;
+    bool isOrange = 0;
 
-    
-    ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+    char initials = 'W'; //WWW
+    char activity = 'P'; //Programming
+    char today = 'F'; //Friday
+
+    int dayOfMonth = 16;
+    int year = 2021;
+    int answer = 5;
+
+    float threeDecimalPlaces = 4.321f;
+    float mypi = 3.13f;
+    float randomDecimal = 3.57618f;
+
+    double fifteenDigits = 4645.45798134586;
+    double threeSigFig = 3.42;
+    double isOverKillFor = 5;
+
+    ignoreUnused(isOn, isHigherThan5, isOrange, initials, activity, today, dayOfMonth, year, answer, threeDecimalPlaces, mypi, randomDecimal, fifteenDigits, threeSigFig, isOverKillFor); //passing each variable declared to the ignoreUnused() function
+
 }
 
 /*
@@ -71,51 +86,95 @@ void variableDeclarations()
  example:
  note: this example shows the result after completing steps 3-8
  */
-bool rentACar(int rentalDuration, int carType = 0)  //function declaration with random number of arguments, arbitrary number of arguments have default value
-{ 
-    ignoreUnused(rentalDuration, carType); //passing each function parameter to the ignoreUnused() function
-    return {}; //if your function returns something other than void, add 'return {};' at the end of it.
-} 
 
 /*
  1)
  */
+double doMaths(float mypi, int randomNumber = 53)
+{ 
+    ignoreUnused(mypi, randomNumber);
+    return {};
+} 
 
 /*
  2)
  */
+char concatenateInitials(char firstName, char secondName = 'M', char lastName)
+{ 
+    ignoreUnused(firstName, secondName, lastName);
+    return {};
+} 
 
 /*
  3)
  */
+void setupProjectView(int windowSizeX = 640, int windowsizeY = 480)
+{ 
+    ignoreUnused(windowSizeX, windowsizeY);
+}
 
 /*
  4)
  */
+bool closeWindow(int mousePosX, int mousePosY)
+{ 
+    ignoreUnused(mousePosX, mousePosY);
+    return {};
+} 
 
 /*
  5)
  */
+bool isWindowOpen(bool sensorState = 0)
+{ 
+    ignoreUnused(sensorState);
+    return {};
+} 
 
 /*
  6)
  */
+void playMusic(int readHeadPosition = 43, char playbackState = 'S')
+{ 
+    ignoreUnused(readHeadPosition, playbackState);
+}
 
 /*
  7)
  */
+int numDogBarksSinceBreakfast(int numDogBarksinHour, double timeSinceBreakfast)
+{ 
+    ignoreUnused(numDogBarksinHour, timeSinceBreakfast);
+    return {};
+} 
 
 /*
  8)
  */
+double tempInDegC(float tempInDegF = 103.5)
+{ 
+    ignoreUnused(tempInDegF);
+    return {};
+} 
 
 /*
  9)
  */
+char theTempTodayIs(char day = 'F', double tempInDegC = 20.1)
+{ 
+    ignoreUnused(day, tempInDegC);
+    return {};
+}
 
 /*
  10)
  */
+bool isThistheLastTask(int currentTaskNum, int totalTaskNum)
+{ 
+    ignoreUnused(currentTaskNum, totalTaskNum);
+    return {};
+}
+
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -133,31 +192,43 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 
 int main()
 {
-    //example of calling that function, storing the value, and passing it to ignoreUnused at the end of main()
-    auto carRented = rentACar(6, 2); 
-    
     //1)
-    
+    auto mathsResult = doMaths(3.13);
+
     //2)
-    
+    auto fullInitials = concatenateInitials('A', 'B', 'C');
+
     //3)
-    
+    //auto viewProject = setupProjectView();
+
+/* this and //6 gave error "[cquery] variable has incomplete type 'void'" */
+
+
     //4)
-    
+    auto closeW = closeWindow(320, 240);
+
     //5)
-    
+    auto openWindow = isWindowOpen();
+  
     //6)
-    
+    //auto play = playMusic(22, 'p');
+
     //7)
-    
+    auto barks = numDogBarksSinceBreakfast(15, 4);
+
     //8)
-    
+    auto theTempTomorrowinDegC = tempInDegC(94);
+
     //9)
-    
+    auto theTempHasGoneUpBy = theTempTodayIs('S', 30);
+ 
     //10)
+    auto amIClose = isThistheLastTask(5, 10);
+
     
-    
-    ignoreUnused(carRented);
+    ignoreUnused(mathsResult, fullInitials, closeW, openWindow, barks, theTempTomorrowinDegC, theTempHasGoneUpBy, amIClose);
+
+
     std::cout << "good to go!" << std::endl;
     return 0;    
 }
